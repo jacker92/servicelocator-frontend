4f74a229723aa5b84cc1c0c0b1da284e7dadf1eb
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-10 18:05:09 +0200

    Added cypress and deploy script

---
+++ cypress/integration/servicelocator.spec.js
@@ -0,0 +1,7 @@
+describe('Service Locator', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Helsinki Service Locator')
+    cy.contains('Search')
+  })
+})
