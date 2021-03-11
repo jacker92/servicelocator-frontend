db7d1df9c87da52c94007147bbbaa83baa882a47
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-11 15:04:03 +0200

    Modified docker-compose

--- src/services/helsinkiService.js
+++ src/services/helsinkiService.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'https://api.hel.fi/servicemap/v2/search/?type=unit&q='
+import dotenv from 'dotenv'
+dotenv.config()
+
+const baseUrl = process.env.REACT_APP_BACKEND_URL
 
 export const getServices = async (searchTerm) => {
   const request = await axios.get(`${baseUrl}${searchTerm}`)
