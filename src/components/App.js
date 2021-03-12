1eb93f7d56ecbf61d26d0e467ee369ba185f5049
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-12 08:51:23 +0200

    Fixed issue with router

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,25 @@ import { Container } from 'react-bootstrap'
 import SearchForm from './SearchForm'
 import ServiceList from './ServiceList'
 import SingleServiceView from './SingleServiceView'
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../App.css'
 
 const App = () => {
   const [services, setServices] = useState([])
 
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <SearchServicesView setServices={setServices} services={services}/>
-        </Route>
-        <Route path="/:id">
-          <SingleServiceView services={services}/>
-        </Route>
-      </Switch>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <SearchServicesView setServices={setServices} services={services}/>
+          </Route>
+          <Route path="/:id">
+            <SingleServiceView services={services}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
