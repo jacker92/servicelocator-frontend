2e4ec299b86680a3f5b0c030606ca28956f2514e
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-12 13:26:30 +0200

    Fixed issue in single service view

--- src/components/SingleServiceView.js
+++ src/components/SingleServiceView.js
@@ -4,7 +4,7 @@ import { Table, Container } from 'react-bootstrap'
 
 const SingleServiceView = ({ services }) => {
   const { id } = useParams()
-  const service = services && services.find(p => p.id === Number(id))
+  const service = services && services.results.find(p => p.id === Number(id))
 
   console.log(services, service)
 
