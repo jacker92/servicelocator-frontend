9db483b73c32025c725a592461b8f7d32b5ca1d0
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-12 09:04:33 +0200

    Added more e2e tests

--- cypress/integration/servicelocator.spec.js
+++ cypress/integration/servicelocator.spec.js
@@ -9,21 +9,33 @@ describe('Service Locator', function() {
     cy.contains('Search')
   })
 
-  it('shows services when requested', function() {
-    cy.get('#searchInput')
-      .type('Laajasalon VPK')
-
-    cy.get('#searchButton')
-      .click()
-
-    cy.get('#serviceTable')
-      .contains('Laajasalon VPK, palokunnantalo')
-  })
-
   it('does not trigger search when submitted empty string', function() {
     cy.get('#searchButton')
       .click()
     cy.get('#serviceTable').should('not.exist')
+  })
+
+  describe('Services', function() {
+
+    beforeEach(function() {
+      cy.get('#searchInput')
+        .type('Laajasalon VPK')
+
+      cy.get('#searchButton')
+        .click()
+    })
+
+    it('shows services when requested', function() {
+      cy.get('#serviceTable')
+        .contains('Laajasalon VPK, palokunnantalo')
+    })
+
+    it('shows single service view when clicked', function() {
+      cy.contains('Laajasalon VPK, palokunnantalo').click()
+      cy.contains('Laajasalon VPK:n perinteikäs palokunnantalo sijaitsee idyllisellä tontilla Laajasalon Yliskylässä.')
+      cy.contains('Yliskylänkaari 10')
+      cy.contains('00840')
+    })
   })
 
 })
